ec51ebb3fa6a58e988b5c4d9fa3e28169324eb1b
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2022-05-24 00:00:44 -0500

    feat: upload via form
    Creation of UploadForm to upload files

--- media_example/views.py
+++ media_example/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
+
 
 # Create your views here.
+import os
+from django.conf import settings
+from django.shortcuts import render
+from .forms import UploadForm
+
+
 def media_example(request):
-	return render(request, "media-example.html")
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            save_path = os.path.join(settings.MEDIA_ROOT, form.cleaned_data["file_upload"].name)
+
+            with open(save_path, "wb") as output_file:
+                for chunk in form.cleaned_data["file_upload"].chunks():
+                    output_file.write(chunk)
+    else:
+        form = UploadForm()
+
+    return render(request, "media-example.html", {"form": form})
