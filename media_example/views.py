b061b9a0945ab2f88bdfa32e64a8fa3385fe26d2
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2022-05-25 00:19:59 -0500

    feat: ImageField
    Implementing ImageField to manage Images with DJango

--- media_example/views.py
+++ media_example/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
+from PIL import Image
 
 
 # Create your views here.
@@ -16,9 +17,9 @@ def media_example(request):
         if form.is_valid():
             save_path = os.path.join(settings.MEDIA_ROOT, form.cleaned_data["file_upload"].name)
 
-            with open(save_path, "wb") as output_file:
-                for chunk in form.cleaned_data["file_upload"].chunks():
-                    output_file.write(chunk)
+            image = Image.open(form.cleaned_data["file_upload"])
+            image.thumbnail((50,50))
+            image.save(save_path)
     else:
         form = UploadForm()
 
