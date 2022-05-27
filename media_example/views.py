9b88be376caf6726c6ce47ae880ec103bcafd068
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2022-05-26 23:10:34 -0500

    feat: ImageField and FileField
    Now the Form can take image and any other type of file.

--- media_example/views.py
+++ media_example/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render
 from django.conf import settings
-import os
-from PIL import Image
 
-
-# Create your views here.
-import os
-from django.conf import settings
 from django.shortcuts import render
 from .forms import UploadForm
+from .models import ExampleModel
 
 
 def media_example(request):
+    instance = None
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            save_path = os.path.join(settings.MEDIA_ROOT, form.cleaned_data["file_upload"].name)
+            instance = ExampleModel()
+            instance.image_field = form.cleaned_data["image_upload"]
+            instance.file_field = form.cleaned_data["file_upload"]
+            instance.save()
 
-            image = Image.open(form.cleaned_data["file_upload"])
-            image.thumbnail((50,50))
-            image.save(save_path)
     else:
         form = UploadForm()
 
-    return render(request, "media-example.html", {"form": form})
+    return render(request, "media-example.html", {"form": form, "instance": instance})
